8f6d03d4a87c026f01b9d59da0829908b23cbfc2
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-07-09 15:51:38 +0700

    - Modified 1 files: [ google-photo/sync.sh]

--- google-photo/sync.sh
+++ google-photo/sync.sh
@@ -5,12 +5,13 @@ REMOTE1="googlephoto-ongtrieuhau861.s24ultra@gmail.com"
 REMOTE2="googlephoto-ongtrieuhau861.s24ultra.001@gmail.com"
 
 # Liệt kê tất cả các album trong tài khoản Google Photos thứ nhất
-albums=$(rclone lsd $REMOTE1:album | awk '{print $5}')
+albums=$(rclone lsd $REMOTE1:album | awk '{$1=$2=$3=""; print substr($0, 4)}')
 
 # Đồng bộ và xóa từng album từ tài khoản thứ nhất sang tài khoản thứ hai
 for album in $albums; do
     echo "Syncing album: $album"
-    # rclone sync $REMOTE1:album/$album $REMOTE2:album/$album
+    # Sử dụng trích dẫn để xử lý tên album có khoảng trắng
+    # rclone sync "$REMOTE1:album/$album" "$REMOTE2:album/$album"
 done
 
 echo "Sync and delete files completed!"
