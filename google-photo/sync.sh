a50382cbdbe48b4cc97cb6c4cfb25cbd89a10e3b
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-07-10 20:51:18 +0700

    - Modified 1 files: [ google-photo/sync.sh]

--- google-photo/sync.sh
+++ google-photo/sync.sh
@@ -2,7 +2,7 @@
 
 # Đặt tên remote cho hai tài khoản Google Photos
 REMOTE1="googlephoto-ongtrieuhau861.s24ultra@gmail.com"
-REMOTE2="googlephoto-ongtrieuhau861.s24ultra.003@gmail.com"
+REMOTE2="googlephoto-ongtrieuhau861.s24ultra.001@gmail.com"
 
 # Liệt kê tất cả các album trong tài khoản Google Photos thứ nhất
 rclone lsjson $REMOTE1:album --dirs-only | jq -r '.[].Path' > albums.txt
@@ -11,10 +11,10 @@ rclone lsjson $REMOTE1:album --dirs-only | jq -r '.[].Path' > albums.txt
 while IFS= read -r album; do
     echo "Syncing album: $album"
     # Thực hiện đồng bộ hóa từ tài khoản thứ nhất sang tài khoản thứ hai
-    # rclone sync "$REMOTE1:album/$album" "$REMOTE2:album/$album"
+    rclone sync "$REMOTE1:album/$album" "$REMOTE2:album/$album"
 done < albums.txt
 
-echo "Syncing album: Ông Triệu Phúc Hiếu"
-rclone sync "$REMOTE1:album/Ông Triệu Phúc Hiếu" "$REMOTE2:album/Ông Triệu Phúc Hiếu"
+# echo "Syncing album: Ông Triệu Phúc Hiếu"
+# rclone sync "$REMOTE1:album/Ông Triệu Phúc Hiếu" "$REMOTE2:album/Ông Triệu Phúc Hiếu"
 
 echo "Sync and delete files completed!"
