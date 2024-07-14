31bef35ccf92ae11c7dc6c164529cb7720780160
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-07-14 14:03:23 +0700

    - Modified 1 files: [ google-photo/sync-media-all.sh]

--- google-photo/sync-media-all.sh
+++ google-photo/sync-media-all.sh
@@ -3,5 +3,5 @@
 # Đặt tên remote cho hai tài khoản Google Photos từ biến môi trường
 remotesource=${remotesource}
 remotedest=${remotedest}
-rclone sync "$remotesource:media/all" "$remotedest:media/all"
+rclone sync "$remotesource:media/all" "$remotedest:upload"
 echo "Sync and delete files completed!"
