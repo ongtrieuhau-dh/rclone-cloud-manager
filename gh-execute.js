acc20abd8bac27c365b7c233f0089cb232c978be
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-08-04 17:59:39 +0700

    - Added 1 files: [ gh-execute.js]  - Modified 1 files: [ .github/workflows/onedrive-moveto.yml]

---
+++ gh-execute.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+var child_process = require("child_process");
+const rJS = require("E:/CLOUDCODE/github.com/oListRepos/NodeJS/oModules/rJS.js");
+const rNode = require("E:/CLOUDCODE/github.com/oListRepos/NodeJS/oModules/rNode.js");
+
+const executeMain = (async () => {
+  const handleError = (error) => {
+    console.error(error.message);
+    throw error;
+  };
+  const execSync = (command = "") => {
+    try {
+      child_process.execSync(command, { cwd: process.cwd(), stdio: "inherit" });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  const login = () => {
+    try {
+      execSync(`gh auth login --with-token < ${__filename}.logintoken.gitignorefile`);
+    } catch (error) {
+      handleError(error);
+    } finally {
+      execSync(`gh auth status`);
+    }
+  };
+  var v = {
+    nameFileTemp: `RCLONE_CONF_BASE64.tmp.gitignorefile`,
+  };
+  v.tempPathBase64 = path.join(__dirname, v.nameFileTemp);
+  try {
+    execSync("gh --version");
+    login();
+    fs.writeFileSync(v.tempPathBase64, fs.readFileSync(path.join(__dirname, "RCLONE_CONF_BASE64.gitignorefile.conf")).toString("base64"));
+    execSync(`gh secret set RCLONE_CONF_BASE64 -b"$(< ${v.nameFileTemp})" --repo ongtrieuhau-dh/rclone-cloud-manager `);
+    execSync(`gh auth logout`);
+  } catch (error) {
+    console.error(`ERR:::${error.message}`);
+  } finally {
+    rNode._LOG(v);
+    try {
+      fs.rmSync(v.tempPathBase64);
+    } catch {}
+  }
+})();
